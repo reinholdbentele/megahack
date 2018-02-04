b9fc349a12908dd3567cd0fb989a632798ff3300
Reinhold <reinhold@bentele.org>
2018-02-04 13:02:31 +0100

    Ready player one?! :-)

--- game.c
+++ game.c
@@ -6,9 +6,22 @@
 
 static uint8_t active_column = 0;
 static uint16_t active_paddle = 0x00ff;
+static uint16_t previous_paddle = 0xffff;
 static bool direction = true;
+static bool game_over = false;
 static uint8_t speed_counter = 0;
 static uint8_t current_speed = 5;
+
+static void new_game(void)
+{
+    active_column = 0;
+    active_paddle = 0x00ff;
+    previous_paddle = 0xffff;
+    direction = true;
+    game_over = false;
+    current_speed = 5;
+    clear_display();
+}
 
 static void move_paddle(void)
 {
@@ -39,6 +52,11 @@ static void move_paddle(void)
 /* Periodic callback, 30 Hz */
 void game_tick(void)
 {
+    if (game_over)
+    {
+        return;
+    }
+
     speed_counter++;
     if (speed_counter < current_speed)
     {
@@ -58,4 +76,22 @@ void game_tick(void)
 void game_button_pressed(void)
 {
     PORTB ^= _BV(LED_GREEN_PIN);
+
+    if (game_over)
+    {
+        new_game();
+        return;
+    }
+
+    active_paddle &= previous_paddle;
+    previous_paddle = active_paddle;
+    display_set_column(active_column, active_paddle);
+    active_column++;
+
+    current_speed = 5 - active_column / 8;
+
+    if ((active_paddle == 0) || (active_column == 32))
+    {
+        game_over = true;
+    }
 }
